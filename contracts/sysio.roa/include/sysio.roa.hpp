22a74862d51123a3276efa24082a99907b8cad1a
dtaghavi <detaghavi@gmail.com>
2024-11-25 20:06:51 +0000

    WIP: .hpp and tables built out, stubbed out ACTIONS
    Got tables added and stubbed out according to our most recent discussions about ROA flow.
    Got all ACTIONs stubbed out to match our plans, will need to add an ACTION to populate nodeowners table.
    addpolicy implemented, needs to be tested.

--- contracts/sysio.roa/include/sysio.roa.hpp
+++ contracts/sysio.roa/include/sysio.roa.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <sysio/sysio.hpp>
-#include <sysio/print.hpp>
+#include <sysio/system.hpp> // For current_block_number
 #include <sysio/singleton.hpp>
 
 namespace sysio {
@@ -9,76 +8,125 @@ namespace sysio {
         public:
             using contract::contract;
             
+            /**
+             * @brief Initializes sysio.roa, should be called as last step in Bios Boot Sequence.
+             * 
+             * @param max_ram_bytes The max amount of bytes the network has. Should match your 'global' table's value in sysio contract. Note: bytes are represented in Base 2.
+             * @param ram_byte_price The cost in SYS for 1 byte of RAM. Value should change on network expansions.
+             */
             [[sysio::action]]
-            void activateroa();
+            void activateroa(const uint64_t& max_ram_bytes, const asset& ram_byte_price);
 
             /**
-            * @brief Add a contract to a T1 Node Owner's whitelist.
-            * 
-            * @param username The T1 Node Owner's username.
-            * @param contract_name The contract's username.
-            * @param cpu The amount of CPU allocated to the contract from T1 Node Owner's pool.
-            * @param net The amount of NET allocated to the contract from T1 Node Owner's pool.
-            * @param ram The amount of RAM allocated to the contract from T1 Node Owner's pool.
-            * @param timelock A timestamp for when the policy expires ( Expires == When the T1 node owner is allowed to delete the policy and free up their allocation )
-            * @return ACTION 
-            */
-            // ACTION addpolicy(const name& username, const name& contract_name, const type& cpu, const type& net, const type& ram, const type& timelock);
+             * @brief Updates the cost of 1 byte of RAM measured in SYS. Requires Node Owner multisig approval, should only be used on network expansion.
+             * 
+             * @param ram_byte_price The NEW cost in SYS for 1 byte of RAM.
+             */
+            [[sysio::action]]
+            void upramcost(const asset& ram_byte_price);
 
             /**
-            * @brief Remove a contract from a T1 Node Owner's whitelist.
-            * 
-            * @param username The T1 Node Owner's username.
-            * @param contract_name The contract's username.
-            * @return ACTION 
-            */
-            // ACTION delpolicy(const name& username, const name& contract_name);
+             * @brief Adds a row to the policies table scoped to 'issuer' and either creates a row in 'reslimit' for 'owner' or increments the values if 'owner' already has a row.
+             * 
+             * @param owner The account to issue this policy for.
+             * @param issuer The Node Owner issuing the policy.
+             * @param net_weight The amount of SYS allocated for NET
+             * @param cpu_weight The amount of SYS allocated for CPU
+             * @param ram_weight The amount of SYS allocated for RAM
+             * @param time_block A block number, the policy can't be deleted or have its values lowered before this block.
+             */
             [[sysio::action]]
-            void adduser(const name& username);
+            void addpolicy(const name& owner, const name& issuer, const asset& net_weight, const asset& cpu_weight, const asset& ram_weight, const uint32_t& time_block);
             
+            /**
+             * @brief Removes resources allocated to 'owner' by 'issuer's policy. Will remove all CPU / NET and UNUSED RAM. Rows will only be removed if all three resources are 0. Only callable after the policies 'time_block' has passed.
+             * 
+             * @param owner The account name that the policy is for.
+             * @param issuer The Node Owner who issued the policy.
+             */
             [[sysio::action]]
-            void  deluser(const name& username);
+            void deletepolicy(const name& owner, const name& issuer);
             
+            // TODO: Think about updating a policy once ram_byte_price has changed. Need to not overwrite this record of price, might be able to only allow "updating" while ram_byte_price in our singleton is the same as when the policy was issued. Would require us to allow multiple rows in 'policies' table with the same 'owner' value, not a huge deal realistically.
+            /**
+             * @brief Updates an already existing policy issued by 'issuer'. If the current block number is less than the policies exisiting 'time_block', values can only be increased. If it is passed 'time_block' values can be modified freely. 
+             * 
+             * Note: RAM that is consumed during the policies life span can't be reclaimed till the RAM is freed up by 'owner', so you can only lower ram_weight to the degree that the RAM isn't consumed. i.e. 10,000 byte allocated and 5,000 bytes were used you can only reclaim 5,000 bytes. 
+             * 
+             * @param owner The account this policy is issued to.
+             * @param issuer The Node Owner who issued this policy.
+             * @param net_weight The new amount of SYS allocated for NET.
+             * @param cpu_weight The new amount of SYS allocated for CPU.
+             * @param ram_weight The new amount of SYS allocated for RAM.
+             * @param time_block The new block number after which this ploicy can be deleted / lowered.
+             */
+            [[sysio:action]]
+            void updatepolicy(const name& owner, const name& issuer, const asset& net_weight, const asset& cpu_weight, const uint64_t& ram_weight, const uint32_t& time_block);
+
+            // TODO: Add an on-notify for registering ( populating ) Node Owners table.
+
         private:
 
+            /**
+             * Config variables for ROA.
+             */
             struct [[sysio::table("roastate")]] roa_state {
                 bool is_active = false;
+                asset ram_byte_price = asset(1, SYS); // TODO: Determine starting value.
 
-                SYSLIB_SERIALIZE(roa_state, (is_active))
+                SYSLIB_SERIALIZE(roa_state, (is_active)(ram_byte_price))
             };
 
-            // Define a singleton type
             typedef sysio::singleton<"roastate"_n, roa_state> roastate_t;
 
             /**
-            * @brief A table scoped by T1 Node Owners account name. Tracks the name of the contract and its alloted resource limits.
-            */
-            // TABLE policies {
-            //     uint64_t policyId;          // Primary key: Unique identifier for the policy
-            //     sysio::name contractName;   // Username of the contract whitelisted
-            //     sysio::asset cpu;           // Allotment of CPU ( % of T1 holders resources )
-            //     sysio::asset net;           // Allotment of NET ( % of T1 holders resources )
-            //     uint64_t ram;               // Allotment of RAM ( % of T1 holders resources )
-            //     sysio::name t1owner;        // T1 Node Owner's username
-            //     uint64_t timeblock;         // Block number when the policy expires
+             * Basic table tracking who T1-3 Node Owners are and their availble vs allocated SYS.
+             */
+            struct [[sysio::table]] nodeowners {
+                name owner;          // Node Owners account name.
+                uint8_t tier;        // Represents what tier they hold: 1, 2, or 3
+                asset total_sys;     // Total SYS alloted based on tier.
+                asset allocated_sys; // Total SYS allocated via policies they issued.
 
-            //     uint64_t primary_key() const { return policyId; }
-            //     uint64_t by_name() const { return contractName.value; }
-            // };
-
-            // typedef multi_index<"policies"_n, policies,
-            //     indexed_by<"byname"_n, const_mem_fun<policies, uint64_t, &policies::by_name>>
-            // > policies_t;
-
-            struct [[sysio::table]] userres {
-                sysio::name username;         // Username of the T1 Node Owner
-                // sysio::asset cpu;      // Total CPU allocated to the user
-                // sysio::asset net;      // Total NET allocated to the user
-                // uint64_t ram;          // Total RAM allocated to the user
-
-                uint64_t primary_key() const { return username.value; }
+                uint64_t primary_key() const { return owner.value; }
+                uint8_t by_tier() const { return tier; }
             };
 
-            typedef sysio::multi_index<"userres"_n, userres> userres_t;
+            typedef multi_index<"nodeowners"_n, nodeowners, 
+                indexed_by<"bytier"_n, const_mem_fun<nodeowners, uint8_t, &nodeowners::by_tier>>
+            > nodeowners_t;
+
+            /**
+             * This table is scoped to Node Owner's account names and is used to track all policies issued by Node Owners.
+             */
+            struct [[sysio::table]] policies {
+                name owner;             // Account name this policy applies to.
+                asset net_weight;       // The amount of SYS allocated for NET.
+                asset cpu_weight;       // The amount of SYS allocated for CPU.
+                asset ram_weight;       // The amount of SYS allocated for RAM.
+                asset ram_byte_price;   // The price, in SYS, for 1 byte of RAM when the policy was created.
+                uint32_t time_block;    // Block number, this policy can't be deleted or have its values lowered before the networks current block num >= time_block.
+
+                uint64_t primary_key() const { return owner.value; }
+            };
+
+            typedef multi_index<"policies"_n, policies> policies_t;
+
+            /**
+             * Holds upper limits of resources an account has access to. This table is used by the Node Operators to maintain usage metrics, replaces 'userres' on sysio.
+             */
+            struct [[sysio::table]] reslimit {
+                name owner;            // Account name this policy applies to
+                asset net_weight;      // Total NET allocated
+                asset cpu_weight;      // Total CPU allocated
+                uint64_t ram_bytes;    // Total RAM allocated
+
+                uint64_t primary_key() const { return owner.value; }
+            };
+
+            typedef sysio::multi_index<"reslimit"_n, reslimit> reslimit_t;
+
+            // ---- Private helper methods ----
+            bool is_roa_active();
     };
 } /// namespace sysio
--- contracts/sysio.roa/src/sysio.roa.cpp
+++ contracts/sysio.roa/src/sysio.roa.cpp
@@ -1,15 +1,8 @@
-#include <sysio.roa.hpp>
+#include <sysio.roa/sysio.roa.hpp>
 
-// ACTION roa::addpolicy(const name& username, const name& contract_name, const type& cpu, const type& net, const type& ram, const type& timelock) {
-
-// }
-
-// ACTION roa::delpolicy(const name& username, const name& contract_name) {
-
-// }
 namespace sysio {
 
-    void roa::activateroa() {
+    void roa::activateroa(const uint64_t& max_ram_bytes, const asset& ram_byte_price) {
         require_auth(get_self());
 
         //Table index
@@ -20,34 +13,162 @@ namespace sysio {
 
         // Check if already initialized.
         check(!state.is_active, "Contract already activated.");
+        
+        // TODO: Might not be needed?
+        // Ensure ram_byte_price is set as SYS token.
+        check(ram_byte_price.symbol == symbol("SYS"), "Ram price must be represented in SYS.");
+
+        // Add sysio and sysio.roa to 'reslimit' table.
+        reslimit_t reslimit(get_self(), get_self().value);
+        reslimit.emplace(get_self(), [&](struct reslimit& a) {
+            a.owner = "sysio.roa"_n;
+            a.net_weight = asset(0, "SYS"); // Set to 0 since system accounts have infinte by default
+            a.cpu_weight = asset(0, "SYS"); // Set to 0 since system accounts have infinite by default
+            a.ram_bytes = max_ram_bytes; // Set to max network bytes.
+        });
+        
+        // TODO: Need to make sure sysio.roa pays for sysio's RAM usage. We can try to have all system account actions paid for by sysio.roa. Or we simply add each of them to reslimit and give them access to RAM.
+        reslimit.emplace(get_self(), [&](struct reslimit& a) {
+            a.owner = "sysio"_n;
+            a.net_weight = asset(0, "SYS"); // Set to 0 since system accounts have infinte by default
+            a.cpu_weight = asset(0, "SYS"); // Set to 0 since system accounts have infinite by default
+            a.ram_bytes = max_ram_bytes; // Set to 0.
+        });
+
+        // TODO: Might need to add a reslimit row for all system accounts. TBD
 
         // Set default values
         state.is_active = true;
+        state.ram_byte_price = ram_byte_price;
 
         // Set values to table.
         state_singleton.set(state, get_self());
     };
 
-    void roa::adduser(const name& username) {
-        userres_t userres(get_self(), get_self().value);
+    void roa::upramcost(const asset& ram_byte_price) {
 
-        auto user_itr = userres.find(username.value);
-        check(user_itr == userres.end(), "Account already added");
+    };
+    
+    // ! Do we want them to submit a block number or a timestamp and then we can determine the block number?
+    void roa::addpolicy(const name& owner, const name& issuer, const asset& net_weight, const asset& cpu_weight, const asset& ram_weight, const uint32_t& time_block) {
+        // Can only issue policies for yourself.
+        require_auth(issuer);
+        
+        //Singelton index
+        roastate_t state_singleton(get_self(), get_self().value);
 
-        userres.emplace(get_self(), [&](struct userres& a) {
-            a.username = username;
+        // Gets values in the table, otherwise creates the row.
+        auto state = state_singleton.get_or_default();
+
+        // Make sure ROA 'is_active' first.
+        check(state.is_active, "ROA is not currently active");
+        
+        nodeowners_t nodeowners(get_self(), get_self().value);
+        auto node_iter = nodeowners.find(issuer.value);
+        // Make sure 'issuer' is actually a Node Owner.
+        check(node_iter != nodeowners.end(), "Only Node Owners can issue policies.");
+
+        // Get a total for the amount of SYS they are trying to allocate.
+        asset total_new_allocation = net_weight + cpu_weight + ram_weight;
+        asset free_sys = node_iter->total_sys - node_iter->allocated_sys;
+
+        // Make sure they have enough unallocated SYS for this new policy.
+        check(total_new_allocation <= free_sys, "Not enough unallocated SYS for this policy.");
+        
+        policies_t policies(get_self(), issuer.value);
+        auto pol_iter = policies.find(owner.value);
+        
+        // Check if 'issuer' already has a policy for 'owner'
+        check(pol_iter == policies.end(), "A policy already exists, use 'updatepolicy'.");
+
+        reslimit_t reslimit(get_self(), get_self().value);
+        auto res_itr = reslimit.find(owner.value);
+
+        // Can't have a negative amount of bytes allocated; ram_weight must be either 0, or enough to cover ATLEAST 1 byte of RAM.
+        check((ram_weight >= state.ram_byte_price || ram_weight == asset(0, "SYS")), "ram_weight must be 0 or greater than ram_byte_price");
+
+        // Calculate allotment of ram_bytes: ram_weight / ram_byte_price.
+        uint64_t ram_bytes = ram_weight / state.ram_byte_price; // TODO: Division results in int64_t ensure this value converts properly. Due to checks we shouldn't ever have negative values so it should map to a uint64 with no issue. Also make sure we have the correct "base" bytes of ram represented in base 2.
+
+        // If they have a row already, this implies that 'owner' has a policy from someone else. Simply increment their 'reslimit' row, or give them a new row if none exists.
+        if (res_itr == reslimit.end()) {
+            reslimit.emplace(get_self(), [&](struct reslimit& a) {
+                a.owner = owner;
+                a.net_weight = net_weight;
+                a.cpu_weight = cpu_weight;
+                a.ram_bytes = ram_bytes;
+                a.issuer = issuer;
+            });
+        } else {
+            // 'owner' already has a 'reslimit' row. Simply increment weights.
+            reslimit.modify(get_self(), [&](struct reslimit& a) {
+                a.net_weight += net_weight;
+                a.cpu_weight += cpu_weight;
+                a.ram_bytes += ram_bytes;
+            });
+        }
+
+        // TODO: Any validation or restrictions on time_block? Minimum, duration? Could add a column to our singleton? We have no way of enforcing some agreed upon duration.
+        // Add a row to Node Owners list of policies.
+        policies.emplace(get_self(), [&](struct policies& a) {
+            a.owner = owner;
+            a.net_weight = net_weight;
+            a.cpu_weight = cpu_weight;
+            a.ram_weight = ram_weight;
+            a.ram_byte_price = state.ram_byte_price;
+            a.time_block = time_block;
+        });
+
+        // Update 'issuer' total allocated SYS.
+        nodeowners.modify(get_self(), [&](struct nodeowners& a) {
+            a.allocated_sys += total_new_allocation;
         });
     };
 
-    void roa::deluser(const name& username) {
+    void roa::deletepolicy(const name& owner, const name& issuer) {
+        require_auth(issuer);
+
         // Instantiate the multi-index table
         userres_t userres(get_self(), get_self().value);
 
         // Find the user in the table
-        auto user_itr = userres.find(username.value);
+        auto user_itr = userres.find(owner.value);
         check(user_itr != userres.end(), "Account not found");
 
         // Erase the user from the table
         userres.erase(user_itr);
+
+        // TODO: Unallocate SYS of 'issuer'
+    };
+
+    void roa::updatepolicy(const name& owner, sysio::asset net_weight, sysio::asset cpu_weight, uint64_t ram_bytes, sysio::name issuer) {
+        require_auth(issuer);
+
+        // TODO: Add checks to ensure 'issuer' has enough unallocated SYS to facilitate this addtional policy consumption.
+
+        userres_t userres(get_self(), get_self().value);
+
+        auto user_itr = userres.find(owner.value);
+        check(user_itr != userres.end(), "Account has no policy to update");
+
+        // TODO: Checks need to be built that ram_bytes CAN NOT be lower than the already consumed RAM.
+        
+        userres.modify(user_itr,get_self(), [&](auto& a) {
+            a.net_weight = net_weight;
+            a.cpu_weight = cpu_weight;
+            a.ram_bytes  = ram_bytes;
+        });
+    };
+
+    // ---- Private helper methods ----
+
+    bool roa::is_roa_active() {
+        //Singelton index
+        roastate_t state_singleton(get_self(), get_self().value);
+
+        // Gets values in the table, otherwise creates the row.
+        auto state = state_singleton.get_or_default();
+
+        return state.is_active;
     };
 }
